4a77a4d933b7084fba69317fcbfbe2a2bf7fa4d6
unknown <akshatsoni7898.com>
2024-08-26 14:24:39 +0530

    third commit

---
+++ src/component/Days.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "../styles/Days.module.css";
+import calLogo from "../images/calendar__logo.png";
+
+const Days = ({ day, time }) => {
+  return (
+    <>
+      <div className={styles.days__content}>
+        <div className={styles.days__logoCal}>
+          <img
+            className={styles.day__calendar}
+            src={calLogo}
+            alt="calendarLogo"
+          />
+        </div>
+        <div className={styles.day__name}>
+          <p className={styles.days__particularDay}>{day}</p>
+          <h5>{time}</h5>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Days;
